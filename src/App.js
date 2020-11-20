0cf0406228f49887149caf70294ae72e095d9c25
Artur Tavares <artur@loba.pt>
2020-11-20 01:22:17 +0000

    5. Intro To Styled Components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import AboutUs from './pages/AboutUs';
+// Global Styled
+import GlobalStyle from './components/GlobalStyle';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
